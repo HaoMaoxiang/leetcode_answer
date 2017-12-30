805908b4eb200121d895163cf971b2e73c0c7909
haomx <haomaoxiang@126.com>
2017-12-30 21:09:26 +0800

    No8_String_to_Integer commit

---
+++ src/No8_String_to_Integer/Solution.java
@@ -0,0 +1,43 @@
+package No8_String_to_Integer;
+
+public class Solution {
+
+    public int myAtoi(String str) {
+        // 1. 判断是否是空字符串
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+        str = str.trim();
+        // 2. 标记正负号
+        boolean positive = true;
+        int i = 0;
+        if (str.charAt(0) == '+') {
+            i++;
+        }
+        if (str.charAt(0) == '-') {
+            i++;
+            positive = false;
+        }
+
+        double result = 0;
+        for (; i < str.length(); i++) {
+            if ((str.charAt(i)-'0' > 9)||(str.charAt(i)-'0' < 0)) {
+                break;
+            } else {
+                result = result * 10 + str.charAt(i)-'0';
+                if (positive && result > Integer.MAX_VALUE) {
+                    return Integer.MAX_VALUE;
+                } else if (!positive && result - 1 > Integer.MAX_VALUE) {
+                    return Integer.MIN_VALUE;
+                }
+            }
+        }
+        return positive ? (int) result : (int) -result;
+
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.myAtoi("-2147483648"));
+    }
+}

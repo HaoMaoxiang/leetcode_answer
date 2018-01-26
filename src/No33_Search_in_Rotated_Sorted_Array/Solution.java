5a493e9fc842b16ce23bf12016eb52a4db75e553
haomx <haomaoxiang@126.com>
2018-01-26 17:04:12 +0800

    No33_Search_in_Rotated_Sorted_Array commit

---
+++ src/No33_Search_in_Rotated_Sorted_Array/Solution.java
@@ -0,0 +1,35 @@
+package No33_Search_in_Rotated_Sorted_Array;
+
+/**
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ * <p>
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ * <p>
+ * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ * <p>
+ * You may assume no duplicate exists in the array.
+ */
+public class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > nums[right]) { //左边有序
+                if (nums[left] <= target & target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {    //右边有序
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}

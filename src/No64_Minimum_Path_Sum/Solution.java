44cfc65e2f418a9075b26eb657bfb91c5f5a8dfc
haomx <haomaoxiang@126.com>
2018-02-01 18:29:24 +0800

    No64_Minimum_Path_Sum commit

---
+++ src/No64_Minimum_Path_Sum/Solution.java
@@ -0,0 +1,43 @@
+package No64_Minimum_Path_Sum;
+
+import javax.xml.transform.SourceLocator;
+
+/**
+ * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+ * <p>
+ * Note: You can only move either down or right at any point in time.
+ * <p>
+ * Example 1:
+ * [[1,3,1],
+ * [1,5,1],
+ * [4,2,1]]
+ * Given the above grid map, return 7. Because the path 1→3→1→1→1 minimizes the sum.
+ */
+public class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = grid[0][i] + dp[0][i-1];
+        }
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = grid[i][0] + dp[i-1][0];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[][] grid = new int[][] {{1,3,1},{1,5,1},{4,2,1}};
+        System.out.println(s.minPathSum(grid));
+    }
+}

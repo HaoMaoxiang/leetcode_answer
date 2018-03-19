8eb399f7966448d51d47088aeaef3125e611d10e
haomx <haomaoxiang@126.com>
2018-03-19 19:56:08 +0800

    No129_Sum_Root_to_Leaf_Numbers commit

---
+++ src/No129_Sum_Root_to_Leaf_Numbers/Solution.java
@@ -0,0 +1,37 @@
+package No129_Sum_Root_to_Leaf_Numbers;
+
+import BasicDataStruct.TreeNode;
+
+/**
+ * Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+ * <p>
+ * An example is the root-to-leaf path 1->2->3 which represents the number 123.
+ * <p>
+ * Find the total sum of all root-to-leaf numbers.
+ * <p>
+ * For example,
+ * <p>
+ * 1
+ * / \
+ * 2   3
+ * The root-to-leaf path 1->2 represents the number 12.
+ * The root-to-leaf path 1->3 represents the number 13.
+ * <p>
+ * Return the sum = 12 + 13 = 25.
+ */
+public class Solution {
+    public int sumNumbers(TreeNode root) {
+        return helper(root, 0);
+    }
+
+    private int helper(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val + sum * 10;
+        }
+
+        return helper(root.left, root.val + sum * 10) + helper(root.right, root.val + sum * 10);
+    }
+}

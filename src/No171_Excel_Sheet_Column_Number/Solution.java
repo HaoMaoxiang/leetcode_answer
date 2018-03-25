5c676a8ded4fc42988bef238265911167660ae56
haomx <haomaoxiang@126.com>
2018-03-25 22:27:24 +0800

    public int titleToNumber(String s) {

---
+++ src/No171_Excel_Sheet_Column_Number/Solution.java
@@ -0,0 +1,26 @@
+package No171_Excel_Sheet_Column_Number;
+
+/**
+ * Related to question Excel Sheet Column Title
+ * <p>
+ * Given a column title as appear in an Excel sheet, return its corresponding column number.
+ * <p>
+ * For example:
+ * <p>
+ * A -> 1
+ * B -> 2
+ * C -> 3
+ * ...
+ * Z -> 26
+ * AA -> 27
+ * AB -> 28
+ */
+public class Solution {
+    public int titleToNumber(String s) {
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            result = result * 26 + (s.charAt(i) - 'A' + 1);
+        }
+        return result;
+    }
+}

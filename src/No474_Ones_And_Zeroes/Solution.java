5e2ce9427ff836da129ea8019afd1fdcef862c39
haomx <haomaoxiang@126.com>
2018-04-13 15:26:21 +0800

    No474_Ones_And_Zeroes commit

---
+++ src/No474_Ones_And_Zeroes/Solution.java
@@ -0,0 +1,51 @@
+package No474_Ones_And_Zeroes;
+
+/**
+ * 在计算机界中，我们总是追求用有限的资源获取最大的收益。
+ * <p>
+ * 现在，假设你分别支配着 m 个 0 和 n 个 1。另外，还有一个仅包含 0 和 1 字符串的数组。
+ * <p>
+ * 你的任务是使用给定的 m 个 0 和 n 个 1 ，找到能拼出存在于数组中的字符串的最大数量。每个 0 和 1 至多被使用一次。
+ * <p>
+ * 注意:
+ * <p>
+ * 给定 0 和 1 的数量都不会超过 100。
+ * 给定字符串数组的长度不会超过 600。
+ * 示例 1:
+ * <p>
+ * 输入: Array = {"10", "0001", "111001", "1", "0"}, m = 5, n = 3
+ * 输出: 4
+ * <p>
+ * 解释: 总共 4 个字符串可以通过 5 个 0 和 3 个 1 拼出，即 "10","0001","1","0" 。
+ * 示例 2:
+ * <p>
+ * 输入: Array = {"10", "0", "1"}, m = 1, n = 1
+ * 输出: 2
+ * <p>
+ * 解释: 你可以拼出 "10"，但之后就没有剩余数字了。更好的选择是拼出 "0" 和 "1" 。
+ */
+public class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (String str : strs) {
+            int ones = 0, zeroes = 0;
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == '0') {
+                    zeroes += 1;
+                } else {
+                    ones += 1;
+                }
+                //str.charAt(i) == '0' ? (zeroes += 1) : (ones += 1);
+            }
+            for (int i = m; i >= zeroes; i--) {
+                for (int j = n; j >= ones; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - zeroes][j - ones] + 1);
+                }
+            }
+        }
+
+        return dp[m][n];
+
+    }
+}
